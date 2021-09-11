098be92a82142c53640b1d76a54595e78da6571b
Jean-André Santoni <jean.andre.santoni@gmail.com>
2021-09-11 11:01:25 +0700

    Sample GLFW program

---
+++ Makefile
@@ -0,0 +1,14 @@
+CFLAGS += -O3 -fPIC -flto -I. -I./include
+LDFLAGS += -lglfw -flto
+
+OBJ = main.o glad.o
+
+%.o: %.c
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+rombundler: $(OBJ)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+clean:
+	rm $(OBJ) rombundler
+
---
+++ main.c
@@ -0,0 +1,47 @@
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	window = glfwCreateWindow(640, 480, "Title", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+	glfwSwapInterval(1);
+
+	glEnable(GL_TEXTURE_2D);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
+
