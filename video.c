be026a9b1f6a7dc2c7aef2bb96676156296d5dee
Jean-André Santoni <jean.andre.santoni@gmail.com>
2021-09-17 10:11:48 +0700

    Disable scanlines for now

--- video.c
+++ video.c
@@ -54,40 +54,40 @@ static const char *g_vshader_src =
 		"gl_Position = vec4(i_pos, 0.0, 1.0) * u_mvp;\n"
 	"}";
 
-// static const char *g_fshader_src =
-// 	"varying vec2 o_coord;\n"
-// 	"uniform sampler2D u_tex;\n"
-// 	"void main() {\n"
-// 		"gl_FragColor = texture2D(u_tex, o_coord);\n"
-// 	"}";
-
 static const char *g_fshader_src =
-	"uniform vec2 u_tex_size;\n"
-	"uniform sampler2D u_tex;\n"
 	"varying vec2 o_coord;\n"
-	"#define BLURSCALEX 0.45\n"
-	"#define LOWLUMSCAN 5.0\n"
-	"#define HILUMSCAN 10.0\n"
-	"#define BRIGHTBOOST 1.25\n"
-	"#define MASK_DARK 0.25\n"
-	"#define MASK_FADE 0.8\n"
+	"uniform sampler2D u_tex;\n"
 	"void main() {\n"
-		"float maskFade = 0.3333*MASK_FADE;\n"
-		"vec2 invDims = 1.0/u_tex_size.xy;\n"
-		"vec2 p = o_coord * u_tex_size;\n"
-		"vec2 i = floor(p) + 0.5;\n"
-		"vec2 f = p - i;\n"
-		"p = (i + 4.0*f*f*f)*invDims;\n"
-		"p.x = mix(p.x , o_coord.x, BLURSCALEX);\n"
-		"float Y = f.y*f.y;\n"
-		"float YY = Y*Y;\n"
-		"float whichmask = fract(o_coord.x*-0.4999);\n"
-		"float mask = 1.0 + float(whichmask < 0.5) * -MASK_DARK;\n"
-		"vec3 colour = texture2D(u_tex, p).rgb;\n"
-		"float scanLineWeight = (BRIGHTBOOST - LOWLUMSCAN*(Y - 2.05*YY));\n"
-		"float scanLineWeightB = 1.0 - HILUMSCAN*(YY-2.8*YY*Y);\n"
-		"gl_FragColor = vec4(colour.rgb*mix(scanLineWeight*mask, scanLineWeightB, dot(colour.rgb,vec3(maskFade))), 1.0);\n"
+		"gl_FragColor = texture2D(u_tex, o_coord);\n"
 	"}";
+
+// static const char *g_fshader_src =
+// 	"uniform vec2 u_tex_size;\n"
+// 	"uniform sampler2D u_tex;\n"
+// 	"varying vec2 o_coord;\n"
+// 	"#define BLURSCALEX 0.45\n"
+// 	"#define LOWLUMSCAN 5.0\n"
+// 	"#define HILUMSCAN 10.0\n"
+// 	"#define BRIGHTBOOST 1.25\n"
+// 	"#define MASK_DARK 0.25\n"
+// 	"#define MASK_FADE 0.8\n"
+// 	"void main() {\n"
+// 		"float maskFade = 0.3333*MASK_FADE;\n"
+// 		"vec2 invDims = 1.0/u_tex_size.xy;\n"
+// 		"vec2 p = o_coord * u_tex_size;\n"
+// 		"vec2 i = floor(p) + 0.5;\n"
+// 		"vec2 f = p - i;\n"
+// 		"p = (i + 4.0*f*f*f)*invDims;\n"
+// 		"p.x = mix(p.x , o_coord.x, BLURSCALEX);\n"
+// 		"float Y = f.y*f.y;\n"
+// 		"float YY = Y*Y;\n"
+// 		"float whichmask = fract(o_coord.x*-0.4999);\n"
+// 		"float mask = 1.0 + float(whichmask < 0.5) * -MASK_DARK;\n"
+// 		"vec3 colour = texture2D(u_tex, p).rgb;\n"
+// 		"float scanLineWeight = (BRIGHTBOOST - LOWLUMSCAN*(Y - 2.05*YY));\n"
+// 		"float scanLineWeightB = 1.0 - HILUMSCAN*(YY-2.8*YY*Y);\n"
+// 		"gl_FragColor = vec4(colour.rgb*mix(scanLineWeight*mask, scanLineWeightB, dot(colour.rgb,vec3(maskFade))), 1.0);\n"
+// 	"}";
 
 static GLuint compile_shader(unsigned type, unsigned count, const char **strings)
 {
